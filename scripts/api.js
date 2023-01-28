d3daf011547313f2dc09673950760e31e475465a
Jose <alex@MacBook-Pro.local>
2023-01-27 23:36:59 -0500

    start api.js

---
+++ scripts/api.js
@@ -0,0 +1,119 @@
+import { store } from './store.js'
+
+export const signUp = (data) => {
+	return fetch(`http://localhost:8000/sign-up`, 
+    {
+		method: 'POST',
+		headers: 
+        {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+}
+
+export const signIn = (data) => {
+	return fetch(`http://localhost:8000/sign-in`, 
+    {
+		method: 'POST',
+		headers: 
+        {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+}
+
+// Player Actions
+export const indexPlayers = () => {
+	return fetch(`http://localhost:8000/players`, 
+    {
+		headers: 
+        {
+			'Authorization': `Bearer ${store.userToken}`,
+		},
+	})
+}
+
+export const showPlayer = (id) => {
+	return fetch(`http://localhost:8000/players/${id}`, 
+    {
+		headers: 
+        {
+			Authorization: `Bearer ${store.userToken}`,
+		},
+	})
+}
+
+export const updatePlayer = (data, id) => {
+	return fetch(`http://localhost:8000/players/${id}`, 
+    {
+		method: 'PATCH',
+		headers: 
+        {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${store.userToken}`,
+		},
+		body: JSON.stringify(data),
+	})
+}
+
+export const deletePlayer = (id) => {
+	return fetch(`http://localhost:8000/players/${id}`, 
+    {
+		method: 'DELETE',
+		headers: 
+        {
+			Authorization: `Bearer ${store.userToken}`,
+		},
+	})
+}
+
+// Team Actions
+export const indexTeam = () => {
+	return fetch('http://localhost:8000/teams', 
+    {
+		headers: 
+        {
+			'Authorization': `Bearer ${store.userToken}`
+		}
+	})
+}
+
+export const showTeam = (id) => {
+	return fetch(`http://localhost:8000/teams/${id}`, 
+    {
+		headers: 
+        {
+			Authorization: `Bearer ${store.userToken}`,
+		},
+	})
+}
+
+export const updateTeam = (data, id) => {
+	return fetch(`http://localhost:8000/teams/${id}`, 
+    {
+		method: 'PATCH',
+		headers: 
+        {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${store.userToken}`,
+		},
+		body: JSON.stringify(data),
+	})
+}
+
+export const deleteTeam = (id) => {
+	return fetch(`http://localhost:8000/teams/${id}`, 
+    {
+		method: 'DELETE',
+		headers: 
+        {
+			Authorization: `Bearer ${store.userToken}`,
+		},
+	})
+}

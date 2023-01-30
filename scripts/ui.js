c6a3047078d8ea8718d66e15e9622d0bfd717f43
Jose <alex@MacBook-Pro.local>
2023-01-30 16:53:12 -0500

    setup  modal

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,8 +1,2 @@
 import { store } from './store.js'
 
-// var modal = document.getElementById("sign-up-modal");
-// var btn = document.getElementById("sign-up-button");
-
-// btn.addEventListener('click', (event) => {
-    
-// })
